09b0086c7319f1e198add4aeadd1a11493e52553
City College <varshasada2005@gmail.com>
2024-11-07 15:20:03 +0530

    first prg

---
+++ assignment1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+    char x=getc(stdin);
+    if(x>='A'&& x<='Z')
+        x=x+32; 
+        else if(x>='a' && x<='z')
+        x=x-32;
+        printf("Value is:%c",x);
+        else
+            printf("invalid input");
+
+
+}
