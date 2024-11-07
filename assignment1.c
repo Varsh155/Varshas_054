62a9c4f9526b417658438b2988e2d8bc95622108
City College <varshasada2005@gmail.com>
2024-11-07 15:31:23 +0530

    first prg

--- assignment1.c
+++ assignment1.c
@@ -2,13 +2,18 @@
 void main()
 {
     char x=getc(stdin);
-    if(x>='A'&& x<='Z')
-        x=x+32; 
-        else if(x>='a' && x<='z')
+    if(x>='A'&& x<='Z'){
+         x=x+32; 
+    }
+    else if(x>='a' && x<='z'){
         x=x-32;
         printf("Value is:%c",x);
-        else
-            printf("invalid input");
-
+    }
+    else{
+        printf("invalid input");
+    }
+           
+    getchar();
+    getchar();
 
 }
